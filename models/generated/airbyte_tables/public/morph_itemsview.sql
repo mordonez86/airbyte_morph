407208df4267e9cc1527062df97d2b158f24e0c0
mordonez <mordonez86@gmail.com>
2022-09-16 12:34:36 -0300

    cambiado nombre de columna price

--- models/generated/airbyte_tables/public/morph_itemsview.sql
+++ models/generated/airbyte_tables/public/morph_itemsview.sql
@@ -23,7 +23,7 @@ select
     catalog_listing,
     initial_quantity,
     available_quantity,
-    price_aibyte_transform,
+    price,
     health_aibyte_transform,
     _airbyte_ab_id,
     _airbyte_emitted_at,
